d2d06e3f5552e2cbe12c97d72c82d62301aa2a74
neto test <andrewquinceee@gmail.com>
2026-05-19 15:16:50 +0300

    fix: move project files to root, clean gitignore

---
+++ src/main/java/ru/practicum/radio/Radio.java
@@ -0,0 +1,53 @@
+package ru.practicum.radio;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public Radio() {
+        this.currentStation = 0;
+        this.currentVolume = 0;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void next() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void setCurrentStation(int newStation) {
+        if (newStation >= 0 && newStation <= 9) {
+            currentStation = newStation;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
---
+++ src/test/java/ru/practicum/radio/RadioTest.java
@@ -0,0 +1,85 @@
+package ru.practicum.radio;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RadioTest {
+
+    @Test
+    void testInitialState() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void testNextStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(5);
+        radio.next();
+        assertEquals(6, radio.getCurrentStation());
+
+        radio.setCurrentStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void testPrevStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(5);
+        radio.prev();
+        assertEquals(4, radio.getCurrentStation());
+
+        radio.setCurrentStation(0);
+        radio.prev();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void testSetStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(0);
+        assertEquals(0, radio.getCurrentStation());
+
+        radio.setCurrentStation(9);
+        assertEquals(9, radio.getCurrentStation());
+
+        radio.setCurrentStation(-1);
+        assertEquals(9, radio.getCurrentStation());
+
+        radio.setCurrentStation(10);
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void testVolumeIncrease() {
+        Radio radio = new Radio();
+
+        radio.increaseVolume();
+        assertEquals(1, radio.getCurrentVolume());
+
+        for (int i = 1; i < 100; i++) {
+            radio.increaseVolume();
+        }
+        assertEquals(100, radio.getCurrentVolume());
+
+        radio.increaseVolume();
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    void testVolumeDecrease() {
+        Radio radio = new Radio();
+
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+
+        for (int i = 0; i < 50; i++) radio.increaseVolume();
+        radio.decreaseVolume();
+        assertEquals(49, radio.getCurrentVolume());
+    }
+}
