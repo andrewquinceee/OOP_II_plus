25c99d5523971b4ba5ad89ee592553a2f3515553
neto test <andrewquinceee@gmail.com>
2026-05-22 09:21:24 +0300

    Radio

--- src/main/java/ru/practicum/radio/Radio.java
+++ src/main/java/ru/practicum/radio/Radio.java
@@ -3,8 +3,16 @@ package ru.practicum.radio;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int numberOfStations;
 
     public Radio() {
+        this.numberOfStations = 10;
+        this.currentStation = 0;
+        this.currentVolume = 0;
+    }
+
+    public Radio(int numberOfStations) {
+        this.numberOfStations = numberOfStations;
         this.currentStation = 0;
         this.currentVolume = 0;
     }
@@ -17,8 +25,12 @@ public class Radio {
         return currentVolume;
     }
 
+    public int getNumberOfStations() {
+        return numberOfStations;
+    }
+
     public void next() {
-        if (currentStation == 9) {
+        if (currentStation == numberOfStations - 1) {
             currentStation = 0;
         } else {
             currentStation++;
@@ -27,14 +39,14 @@ public class Radio {
 
     public void prev() {
         if (currentStation == 0) {
-            currentStation = 9;
+            currentStation = numberOfStations - 1;
         } else {
             currentStation--;
         }
     }
 
     public void setCurrentStation(int newStation) {
-        if (newStation >= 0 && newStation <= 9) {
+        if (newStation >= 0 && newStation < numberOfStations) {
             currentStation = newStation;
         }
     }
